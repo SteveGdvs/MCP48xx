feebb9554e9a3e345bcdf2ccab3db48fc4fe4125
Steve Gkountouvas <stgountouvas@gmail.com>
2019-10-29 20:24:26 +0200

    Basic functionality of setting the value of the command register

--- MCP4822.cpp
+++ MCP4822.cpp
@@ -1,1 +1,75 @@
 #include "MCP4822.h"
+
+MCP4822::MCP4822(uint8_t cs) : cs(cs) {
+    /* Setting channel bits*/
+    command[MCP4822Channel::A] = command[MCP4822Channel::A] | (1u << 15u);
+    command[MCP4822Channel::B] = command[MCP4822Channel::B] | (0u << 15u);
+
+    turnOnChannel(MCP4822Channel::A);
+    turnOnChannel(MCP4822Channel::B);
+    setGain(MCP4822Gain::High, MCP4822Channel::A);
+    setGain(MCP4822Gain::High, MCP4822Channel::B);
+}
+
+void MCP4822::init() {
+    SPI.begin();
+    pinMode(cs, OUTPUT);
+    digitalWrite(cs, HIGH);
+}
+
+void MCP4822::setVoltage(uint16_t value, MCP4822Channel channel) {
+    if (value > 4095) {
+        value = 4095;
+    }
+    command[channel] = command[channel] | value;
+}
+
+void MCP4822::setVoltageA(uint16_t value) {
+    if (value > 4095) {
+        value = 4095;
+    }
+    command[MCP4822Channel::A] = command[MCP4822Channel::A] | value;
+}
+
+void MCP4822::setVoltageB(uint16_t value) {
+    if (value > 4095) {
+        value = 4095;
+    }
+    command[MCP4822Channel::B] = command[MCP4822Channel::B] | value;
+}
+
+void MCP4822::shutdownChannel(MCP4822Channel channel) {
+    command[channel] = command[channel] | (0u << 12u);
+}
+
+void MCP4822::shutdownChannelA() {
+    command[MCP4822Channel::A] = command[MCP4822Channel::A] | (0u << 12u);
+}
+
+void MCP4822::shutdownChannelB() {
+    command[MCP4822Channel::B] = command[MCP4822Channel::B] | (0u << 12u);
+}
+
+void MCP4822::turnOnChannel(MCP4822Channel channel) {
+    command[channel] = command[channel] | (1u << 12u);
+}
+
+void MCP4822::turnOnChannelA() {
+    command[MCP4822Channel::A] = command[MCP4822Channel::A] | (1u << 12u);
+}
+
+void MCP4822::turnOnChannelB() {
+    command[MCP4822Channel::B] = command[MCP4822Channel::B] | (1u << 12u);
+}
+
+void MCP4822::setGain(MCP4822Gain gain, MCP4822Channel channel) {
+    command[channel] = command[channel] | (gain << 13u);
+}
+
+void MCP4822::setGainA(MCP4822Gain gain) {
+    command[MCP4822Channel::A] = command[MCP4822Channel::A] | (gain << 13u);
+}
+
+void MCP4822::setGainB(MCP4822Gain gain) {
+    command[MCP4822Channel::B] = command[MCP4822Channel::B] | (gain << 13u);
+}
--- MCP4822.h
+++ MCP4822.h
@@ -1,4 +1,53 @@
 #ifndef MCP4822_LIB_MCP4822_H
 #define MCP4822_LIB_MCP4822_H
 
+#include <Arduino.h>
+#include <SPI.h>
+
+enum MCP4822Channel : uint8_t {
+    A = 0u, B = 1u
+};
+enum MCP4822Gain : uint8_t {
+    High = 0u, Low = 1u
+};
+
+class MCP4822 {
+
+
+private:
+    const uint8_t cs;
+    uint16_t command[2]= {0};
+
+public:
+    explicit MCP4822(uint8_t cs);
+
+    void init();
+
+    void setVoltage(uint16_t value, MCP4822Channel channel);
+
+    void setVoltageA(uint16_t value);
+
+    void setVoltageB(uint16_t value);
+
+    void shutdownChannel(MCP4822Channel channel);
+
+    void shutdownChannelA();
+
+    void shutdownChannelB();
+
+    void turnOnChannel(MCP4822Channel channel);
+
+    void turnOnChannelA();
+
+    void turnOnChannelB();
+
+    void setGain(MCP4822Gain gain, MCP4822Channel channel);
+
+    void setGainA(MCP4822Gain gain);
+
+    void setGainB(MCP4822Gain gain);
+
+};
+
+
 #endif //MCP4822_LIB_MCP4822_H
