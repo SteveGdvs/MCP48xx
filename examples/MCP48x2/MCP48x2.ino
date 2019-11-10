ee977b3bdeed525f7115c9d41024fc29023c3d1b
Steve Gkountouvas <stgountouvas@gmail.com>
2019-11-10 04:31:26 +0200

    Updated examples

--- examples/MCP48x2/MCP48x2.ino
+++ examples/MCP48x2/MCP48x2.ino
@@ -1,41 +1,45 @@
 /*
-    This sketch demonstrates how we can output a value in channel A of the MCP4822
-    and cycle  through different values in a loop
+    This sketch demonstrates how we can output a value in both channels of MCP4822 or MCP4812 or MCP4802.
 */
 
 #include <MCP48xx.h>
 
 // Define the MCP4822 instance, giving it the SS (Slave Select) pin
-MCP48xx dac(10);
+// The constructor will also initialize the SPI library
+// We can also define a MCP4812 or MCP4802
+MCP4822 dac(10);
 
-// We define an int variable to store the voltage in mV
+// We define an int variable to store the voltage in mV so 100mV = 0.1V
 int voltage = 100;
 
 void setup() {
     // We call the init() method to initialize the instance
-    // The init method will laso initialize the SPI library
+
     dac.init();
 
     // The channels are turned off at startup so we need to turn the channel we need on
     dac.turnOnChannelA();
+    dac.turnOnChannelB();
 
-    // We configure the channel A in High gain
+    // We configure the channels in High gain
     // It is also the default value so it is not really needed
     dac.setGainA(MCP4822::High);
+    dac.setGainB(MCP4822::High);
 }
 
+// We loop from 100mV to 2000mV for channel A and 4000mV for channel B
 void loop() {
-    // We set channel A to output the value in voltage
+    // We set channel A to output 500mV
     dac.setVoltageA(voltage);
+
+    // We set channel B to output 1000mV
+    dac.setVoltageB(voltage * 2);
 
     // We send the command to the MCP4822
     // This is needed every time we make any change
     dac.updateDAC();
 
-
-    // The maximux value that the MCP4822 can output is 4095 mV
-    // so when we go higher we start again from 100 mV
-    if (voltage > 4095) {
+    if (voltage * 2 > 4000) {
         voltage = 100;
     }
 
